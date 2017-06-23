004d41ac0213f92ee22107459196ee31be7955eb
Darren Cooney <dcooney@ecentricarts.com>
2017-06-23 12:10:59 -0400

    Updates

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,1 +1,7 @@
-"use strict";
+'use strict';
+
+// Open links in new window.
+var links = document.querySelectorAll('a');
+links.forEach(function (link) {
+  return link.target = '_blank';
+});
---
+++ src/js/functions.js
@@ -0,0 +1,5 @@
+
+
+// Open links in new window.
+const links = document.querySelectorAll('a');
+links.forEach(link => link.target = '_blank' );
