44dca7b1e255dca11f8b720198e2f9f0c615c0cd
Darren Cooney <darren.cooney@gmail.com>
2020-02-20 08:32:59 -0500

    Updated build

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,17 +1,16 @@
-const webpack = require('webpack');
-const config = require('../webpack.config.js');
+const webpack = require("webpack");
+const config = require("../webpack.config.js");
 
 config.watch = false;
 config.entry = {
-		'functions.min': './src/js/functions.js',
-		'screen.min': './src/scss/screen.scss'
-	},
-
-	config.plugins.push(
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		})
-	);
+	functions: "./src/js/functions.js",
+	screen: "./src/scss/screen.scss"
+};
+config.plugins.push(
+	new webpack.DefinePlugin({
+		"process.env": {
+			NODE_ENV: '"production"'
+		}
+	})
+);
 module.exports = config;
